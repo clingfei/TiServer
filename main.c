a605ab81d8acd2b37b37980d0a32fb155d64e290
clingfei <1599101385@qq.com>
2021-03-21 19:46:11 +0800

    partly finished post function

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include "csapp.h"
 
-void process(connectfd);
+void process(int connectfd);
 void read_requesthdrs(rio_t *rp);
 int parse_url(char *url, char *filename, char *cgiargs);
 void serve_static(int connectfd, char *filename, size_t size);
 void client_error(int connectfd, char *errCode, char *errMsg);
-void read_post_headers(rio_t *rp);
-void serve_post(int connectfd, char *buf, size_t size);
-void receive(rio_t *rp, char *buf);
+void read_requestbody(rio_t *rp, char *buf);
+void serve_dynamic(int connectfd, char *filename, char *cgiargs);
+void serve_post(int connectfd);
+void get_filetype(char *filename, char *filetype);
+void serve_get(int connectfd, char *filename, int is_static, char *cgiargs);
 
 int main(int argc, char **argv) {
     int listenfd, connectfd;
@@ -28,8 +30,7 @@ int main(int argc, char **argv) {
     }
 }
 
-void process(connectfd) {
-    struct stat sbuf;
+void process(int connectfd) {
     rio_t rio;
     char buf[MAXLINE];
     char method[MAXLINE], url[MAXLINE], version[MAXLINE];
@@ -39,84 +40,75 @@ void process(connectfd) {
     Rio_readinitb(&rio, connectfd);
     Rio_readlineb(&rio, buf, MAXLINE);
     sscanf(buf, "%s %s %s", method, url, version);
+    if(strcasecmp(method, "GET")!=0 && strcasecmp(method, "POST")!=0) {             //仅支持POST和GET请求
+        client_error(connectfd, "501", "Request method is not supported.");
+        return;
+    }
+    printf("method:%s\r\n", method);
+
     is_static = parse_url(url, filename, cgiargs);
+    read_requesthdrs(&rio);
+    printf("%s \r\n", filename);
+
+    if(strcasecmp(method, "POST")==0){
+        printf("method:%s\r\n", method);
+        read_requesthdrs(&rio);
+        serve_post(connectfd);
+        return;
+    }
+    else{
+        serve_get(connectfd, filename, is_static, cgiargs);
+        return;
+    }
+}
+
+void serve_get(int connectfd, char *filename, int is_static, char *cgiargs) {               //处理GET请求
+    struct stat sbuf;
+    if(stat(filename, &sbuf)<0) {                   //将filename所指向的文件读入sbuf
+        client_error(connectfd, "404", "File not found.");
+        return;
+    }
+
     if(is_static) {
-        if(!strcasecmp(method, "GET")) {
-            read_requesthdrs(&rio);                 //read requested head;
-
-            if(stat(filename, &sbuf)<0) {                   //将filename所指向的文件读入sbuf
-                printf("%s", filename);
-                printf("testclient\n");
-
-                client_error(connectfd, "404", "File not found.");
-                printf("test client\r\n");
-                return;
-            }
-            printf("Ssss\n");
-            if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
-                //clienterror(connectfd, filename, "403", "Tiny couldn't read the file");
-                return;
-            }
-            serve_static(connectfd, filename, sbuf.st_size);
+        printf("static\r\n");
+        if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+            client_error(connectfd, "403", "Tiny couldn't read the file");
             return;
         }
-        else if(!strcasecmp(method, "POST")) {
-            char buf[MAXBUF];
-            read_post_headers(&rio);
-            receive(&rio, buf);
-            serve_post(connectfd, buf, sizeof(buf));
-        }
-        else {
-            client_error(connectfd, "501", "Method not supported.");
-        }
+        serve_static(connectfd, filename, sbuf.st_size);
+        return;
+    }
+    else {
+        printf("not static\r\n");
+        serve_dynamic(connectfd, filename, cgiargs);
     }
 }
 
-void read_post_headers(rio_t *rp) {
-    printf("Method: POST\r\n");
+void serve_post(int connectfd) {
+    char buf[MAXBUF], body[MAXBUF];
+    sprintf(body, "<html><title>POST</title></head>" );
+    sprintf(body, "Upload Successfully");
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    Rio_writen(connectfd, buf, strlen(buf));
+    sprintf(buf, "Content-type: text/html\r\n");
+    Rio_writen(connectfd, buf, strlen(buf));
+    sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+    Rio_writen(connectfd, buf, strlen(buf));
+    Rio_writen(connectfd, body, (int)strlen(body));
+}
+
+void read_requestbody(rio_t *rp, char *cgiargs){
     char buf[MAXLINE];
     Rio_readlineb(rp, buf, MAXLINE);
-    while(strcmp(buf, "\r\n")){
+    strcat(cgiargs, buf);
+    while(strcmp(buf, "\r\n")) {
         Rio_readlineb(rp, buf, MAXLINE);
-        printf("%s", buf);
+        strcat(cgiargs, buf);
     }
-
-    printf("test");
-
-    Rio_readlineb(rp, buf, MAXLINE);
-    //while(strcmp(buf, "\r\n")) {
-    //    Rio_readlineb(rp, buf, MAXLINE);
-    //    printf("%s", buf);
-    //}
-    //strcpy(buf, "ssss");
-    //Rio_writen(connectfd, buf, strlen(buf));
     return;
 }
 
-void receive(rio_t *rp, char *buf) {
-    char body[MAXLINE];
-    Rio_readlineb(rp, body, MAXLINE);
-    sprintf(buf, "%s", body);
-    //while(strcmp(body, "\r\n")) {
-    //    Rio_readlineb(rp, body, MAXLINE);
-    //    sprintf(buf, "%s%s", buf, body);
-    //}
-    strcpy(buf, "ssss");
-    return;
-}
-
-void serve_post(int connectfd, char *buf, size_t size) {
-    sprintf(buf, "HTTP/1.0 200 OK\r\n");
-    sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
-    sprintf(buf, "%sContent-length: %d\r\n", buf, size);
-    sprintf(buf, "%sContent-type: %s\r\n\r\n", buf);
-    Rio_writen(connectfd, buf, strlen(buf));
-
-    Rio_writen(connectfd, buf, size);
-    return;
-}
-
-void read_requesthdrs(rio_t *rp) {
+void read_requesthdrs(rio_t *rp) {              //读取请求头，在后台输出
     printf("Method: GET");
     char buf[MAXLINE];
     Rio_readlineb(rp, buf, MAXLINE);
@@ -134,13 +126,16 @@ int parse_url(char *url, char *filename, char *cgiargs) {
         strcpy(cgiargs, "");
         strcpy(filename, ".");
         strcat(filename, url);
-        if(url[strlen(url)-1] == "/") {
+        printf("%s\r\n", url);
+        printf("%d", strlen(url)-1);
+        if(url[strlen(url)-1] == '/') {
             strcat(filename, "index.html");
         }
+        printf("%s \r\n", filename);
         return 1;
     }
     else {
-        p = index(url, '?');          //找到？首次出现
+        p = index(url, '?');          //找到？首次出现的位置
         if (p) {
             strcpy(cgiargs, p+1);
             *p = '\0';
@@ -156,6 +151,7 @@ int parse_url(char *url, char *filename, char *cgiargs) {
 void serve_static(int connectfd, char *filename, size_t size) {
     int srcfd;
     char *srcp, buf[MAXBUF], filetype[MAXLINE];
+    get_filetype(filename, filetype);
 
     sprintf(buf, "HTTP/1.0 200 OK\r\n");
     sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
@@ -181,7 +177,7 @@ void get_filetype(char *filename, char *filetype) {
         strcpy(filetype, "text/plain");
 }
 
-void client_error(int connectfd, char *errCode, char *errMsg) {
+void client_error(int connectfd, char *errCode, char *errMsg) {             //向用户输出错误信息
     printf("step in client\r\n");
     char buf[MAXBUF], body[MAXBUF];
     sprintf(body, "<html><title>Error</title></head>" );
@@ -196,4 +192,20 @@ void client_error(int connectfd, char *errCode, char *errMsg) {
     sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
     Rio_writen(connectfd, buf, strlen(buf));
     Rio_writen(connectfd, body, (int)strlen(body));
+}
+
+void serve_dynamic(int connectfd, char *filename, char *cgiargs) {          //处理动态请求
+    char buf[MAXLINE], *emptylist[] = { NULL };
+
+    sprintf(buf, "HTTP/1.0 200 OK\r\n");
+    Rio_writen(connectfd, buf, strlen(buf));
+    sprintf(buf, "Server: Tiny Web Server\r\n");
+    Rio_writen(connectfd, buf, strlen(buf));
+
+    if (Fork() == 0) {
+        setenv("QUERY_STRING", cgiargs, 1);
+        Dup2(connectfd, STDOUT_FILENO);
+        Execve(filename, emptylist, environ);
+    }
+    Wait(NULL);
 }
